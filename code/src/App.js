55f65ede9e6afccf94d2e1aa161b029e9b56c68d
sarah wolfson <swolfson23@gmail.com>
2020-03-30 11:13:56 +0200

    updated to message to repost

--- code/src/App.js
+++ code/src/App.js
@@ -1,49 +1,56 @@
-import React, { useEffect, useState } from "react"
-import { HappyThought } from "./components/HappyThought"
-import { HappyForm } from "./components/HappyForm"
+import React, { useEffect, useState } from "react";
+import { HappyThought } from "./components/HappyThought";
+import { HappyForm } from "./components/HappyForm";
 
-const url = "https://technigo-thoughts.herokuapp.com/"
+const url = "https://technigo-thoughts.herokuapp.com/";
 
 // new thought & postmessage
 export const App = () => {
-  const [thoughts, setThoughts] = useState([])
-  const [postedMessage, setPostedMessage] = useState("")// needed to refetch the array with new message
-
+  const [thoughts, setThoughts] = useState([]);
+  const [postedMessage, setPostedMessage] = useState("");
 
   // Fetching and listing the json in  the setThoughts
   useEffect(() => {
     fetch(url)
       .then(res => res.json())
-      .then(json => setThoughts(json))
-
-  }, [postedMessage])
+      .then(json => setThoughts(json));
+  }, [postedMessage]);
 
   const onFormSubmit = message => {
-    setPostedMessage(message) //Added to message to postmessage  form to the array of thouhgts without  using the fetch again
-  }
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({ message }),
+      headers: { "Content-Type": "application/json" }
+    })
+      .then(() => setPostedMessage(message))
+      .catch(err => console.log("error:", err));
+  };
 
   const onLikedClick = thoughtId => {
-    console.log(thoughtId)
+    console.log(thoughtId);
 
     const updateThought = () =>
       thoughts.map(thought => {
-        if (thought._id === thoughtId) {  //setting the id
-          thought.hearts += 1
+        if (thought._id === thoughtId) {
+          thought.hearts += 1;
         }
-        return thought // Returning the new thought.
-      })
-    setThoughts(updateThought) // updates
-  }
+        return thought;
+      });
+    setThoughts(updateThought);
+  };
 
   return (
     <section>
       <HappyForm onFormSubmit={onFormSubmit} />
       <div className="container">
         {thoughts.map(thought => (
-          <HappyThought key={thought._id} thought={thought} onLikedClick={onLikedClick} />
+          <HappyThought
+            key={thought._id}
+            thought={thought}
+            onLikedClick={onLikedClick}
+          />
         ))}
       </div>
-
     </section>
-  )
-}
+  );
+};
